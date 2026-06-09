5565431e7a324fce7473d235d5e1669cf647933a
herix <herix@LAPTOP-DREDF63C>
2026-06-09 16:46:09 +0900

    エンティティを追加

--- AssignHub/src/main/java/com/assignhub/entity/Company.java
+++ AssignHub/src/main/java/com/assignhub/entity/Company.java
@@ -16,6 +16,42 @@ public class Company {
 
 	/** 企業名 */
 	private String companyName;
+	
+	/** 企業名カナ */
+	private String compNameKana;
+	
+	/** 郵便番号 */
+	private String compZipCode;
+	
+	/** 住所1 */
+	private String compAddress1;
+	
+	/** 住所2 */
+	private String compAddress2;
+	
+	/** TEL */
+	private String compTel;
+	
+	/** FAX */
+	private String fax;
+	
+	/** 設立年度 */
+	private int foundedYear;
+	
+	/** 社員数 */
+	private int employeeCount;
+	
+	/** 代表者姓 */
+	private String repLastName;
+	
+	/** 代表者姓カナ */
+	private String repLastNameKana;
+	
+	/** 代表者名 */
+	private String repFirstName;
+	
+	/** 代表者名カナ */
+	private String repFirstNameKana;
 
 	/** 作成日時 */
 	private LocalDateTime createdAt;
