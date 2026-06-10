a004ef347f50b3fe1054cbe634efa3f1905ba0f0
土手内奨 <@192.168.6.50>
2026-06-10 15:41:52 +0900

    社員情報のmapper RootControllerの追記

--- AssignHub/src/main/java/com/assignhub/controller/RootController.java
+++ AssignHub/src/main/java/com/assignhub/controller/RootController.java
@@ -18,6 +18,6 @@ public class RootController {
      */
     @GetMapping("/")
     public String root() {
-        return "redirect:/employees";
+        return "redirect:/deleted-accouts";
     }
 }
