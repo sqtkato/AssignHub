419de019c317f3951ed96be90c45dd27175f64a0
c3s_216 <c3s_216@LAPTOP-0CP47KJA>
2026-06-10 10:28:09 +0900

    坂田　export一部更新

--- AssignHub/src/main/java/com/assignhub/controller/AccountController.java
+++ AssignHub/src/main/java/com/assignhub/controller/AccountController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
