f00a1bfc87592f0e6f03127728d09c09060e8696
c3s_216 <c3s_216@LAPTOP-0CP47KJA>
2026-06-11 13:38:16 +0900

    layout,style変更

--- AssignHub/src/main/java/com/assignhub/service/AccountService.java
+++ AssignHub/src/main/java/com/assignhub/service/AccountService.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import com.assignhub.entity.Account;
@@ -74,7 +73,7 @@ public class AccountService {
 	        passwordEncoder.encode(account.getPasswordHash())
 	    );
 
-		accountMapper.save(account);
+		
 		
 	}
 	
