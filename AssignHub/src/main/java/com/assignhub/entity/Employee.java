8c53a680fd16c5c2d238bd73c03081d4ae6577df
c3s_218 <c3s_218@LAPTOP-Q9LDQPEP>
2026-06-11 15:31:19 +0900

    社員情報Entity名称更新、不要なEntity削除

--- AssignHub/src/main/java/com/assignhub/entity/Employee.java
+++ AssignHub/src/main/java/com/assignhub/entity/Employee.java
@@ -4,26 +4,75 @@ import java.time.LocalDateTime;
 
 import lombok.Data;
 
-/**
- * 社員（エンジニア）情報を保持するエンティティクラス。
- * データベースの m_employee テーブルに対応する。
- * * @version 1.01 2026/06/01
- * @author SQT）チームB
- */
 @Data
 public class Employee {
 	/** 社員ID（主キー） */
 	private Integer empId;
 
+	/** 社員姓 */
+	private String lastName;
+
 	/** 社員名 */
-	private String empName;
-
-	/** 入社年 */
-	private Integer hireYear;
-
+	private String firstName;
+	
+	/** 社員姓カナ */
+	private String lastNameKana;
+	
+	/** 社員名カナ */
+	private String firstNameKana;
+	
+	/** 入社年月日 */
+	private String hireDate;
+	
+	/** 勤続年数 */
+	private Integer yearsOfService;
+	
+	/** 生年月日 */
+	private String BirthDate;
+	
+	/** 郵便番号 */
+	private String zipCode;
+	
+	/** 住所1 */
+	private String address1;
+	
+	/** 住所2 */
+	private String address2;
+	
+	/** エンジニアタイプ */
+	private String engineerType;
+	
+	/** アカウントID */
+	private Integer accountId;
+	
+	/** 企業ID */
+	private Integer companyId;
+	
+	/** 所属先企業 */
+	private String companyName;
+	
+	/** アサイン先企業 */
+	private String companyNameAssign;
+	
+	/** 所属部署 */
+	private String department;
+	
+	/** 役職 */
+	private String jobTitle;
+	
+	/** 電話番号 */
+	private String empTel;
+	
+	/** メールアドレス */
+	private String email;
+	
+	/** 削除フラグ */
+	private Integer deleteFlg;
+	
 	/** 作成日時 */
 	private LocalDateTime createdAt;
 
 	/** 更新日時 */
 	private LocalDateTime updatedAt;
+
 }
