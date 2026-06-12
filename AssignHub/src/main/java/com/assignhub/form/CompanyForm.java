4ffa9d20ad6ae9c6f498d418f3022439d29258c5
Administrator <Administrator@APCraft-NP006>
2026-06-12 13:58:48 +0900

    トゥ

--- AssignHub/src/main/java/com/assignhub/form/CompanyForm.java
+++ AssignHub/src/main/java/com/assignhub/form/CompanyForm.java
@@ -2,6 +2,7 @@ package com.assignhub.form;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -16,7 +17,6 @@ import lombok.Data;
 public class CompanyForm {
 
 	/** 企業ID（更新処理の際に対象を特定するために使用、新規登録時はnull） */
-	@NotBlank(message = "企業IDは必須です")
 	private Integer companyId;
 
 	/** 企業名（必須、最大50文字） */
@@ -29,7 +29,6 @@ public class CompanyForm {
 	private String companyNameKana;
 	
 	@NotBlank(message = "郵便番号は必須です")
-	@Size(max = 7, message = "郵便番号は7桁以内で入力してください")
 	private String companyZipCode;
 	
 	@NotBlank(message = "住所１は必須です")
@@ -40,16 +39,16 @@ public class CompanyForm {
 	private String companyAddress2;
 	
 	@NotBlank(message = "電話番号は必須です")
-	@Size(max = 11, message = "電話番号10桁または11桁で入力してください")
 	private String companyTel;
 	
 	@Size(max = 20, message = "FAX番号は20文字以内で入力してください")
 	private String fax;
 	
-	@Size(max = 4, message = "設立年度は4桁で入力してください")
+	@Digits(integer = 4, fraction = 0, message = "設立年度は4桁で入力してください")
 	private Integer foundedYear;
 	
-	@Size(max = 5, message = "社員数は5桁以内で入力してください")
+	
+	@Digits(integer = 5, fraction = 0, message = "社員数は5桁以内で入力してください")
 	private Integer employeeCount;
 	
 	@Size(max = 50, message = "代表者姓は50文字以内で入力してください")
