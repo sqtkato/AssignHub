e35a68930acec646be1168fbe86ca4a502cf520b
NonCa <NonCa@LAPTOP-8RM03O58>
2026-06-12 17:32:36 +0900

    RoleService 変更

--- AssignHub/src/main/java/com/assignhub/service/RoleService.java
+++ AssignHub/src/main/java/com/assignhub/service/RoleService.java
@@ -2,9 +2,15 @@ package com.assignhub.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.assignhub.entity.Role;
 import com.assignhub.mapper.RoleMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class RoleService {
 	private final RoleMapper roleMapper;
 
