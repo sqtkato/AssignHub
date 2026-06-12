c334e45c47972d7c6026f86c2bd9160a437932d5
alath <alath@LAPTOP-MB0TCE90>
2026-06-12 11:23:49 +0900

    Company entity修正

--- AssignHub/src/main/java/com/assignhub/entity/Company.java
+++ AssignHub/src/main/java/com/assignhub/entity/Company.java
@@ -16,10 +16,50 @@ public class Company {
 
 	/** 企業名 */
 	private String companyName;
+	
+	/** 企業名カナ */
+	private String compNameKana;
+	
+	/** 郵便番号 */
+	private String companyZipCode;
+	
+	/** 住所1 */
+	private String companyAddress1;
+	
+	/** 住所2 */
+	private String companyAddress2;
+	
+	/** TEL */
+	private String companyTel;
+	
+	/** FAX */
+	private String fax;
+	
+	/** 設立年度 */
+	private Integer foundedYear;
+	
+	/** 社員数 */
+	private Integer employeeCount;
+	
+	/** 代表者姓 */
+	private String repLastName;
+	
+	/** 代表者姓カナ */
+	private String repLastNameKana;
+	
+	/** 代表者名 */
+	private String repFirstName;
+	
+	/** 代表者名カナ */
+	private String repFirstNameKana;
 
 	/** 作成日時 */
 	private LocalDateTime createdAt;
 
 	/** 更新日時 */
 	private LocalDateTime updatedAt;
+	
+	/** 削除フラグ*/
+	private Integer deleteFlg;
+	
 }
