a903b8c7616197ce9926ddb9091393c433341175
CRN <CRN@CRN_P058>
2026-06-16 11:59:44 +0900

    ポケモンに合わせる形で修正。500ケンのエラーはまだやってません。

--- AssignHub/src/main/java/com/assignhub/controller/DeletedAccountController.java
+++ AssignHub/src/main/java/com/assignhub/controller/DeletedAccountController.java
@@ -108,30 +108,40 @@ public class DeletedAccountController {
     }
 	
 //	エクスポート画面へ遷移	
-	@GetMapping("/export")
+	@PostMapping("/export")
 	public String showExport(@RequestParam(name = "ids", required = false) List<Integer> ids, Model model, RedirectAttributes attributes) {		
 		// チェックがなければ一覧へ戻す
 	    if (ids == null || ids.isEmpty()) {
 	        attributes.addFlashAttribute("toastError", "エクスポートする対象が選択されていません。");
 	        return "redirect:/deleted-accounts";
 	    }		
-		model.addAttribute("count", deletedAccountService.findByIds(ids));
-		
+	    List<Account> accounts = deletedAccountService.findByIds(ids);
+		// 取得した件数とリスト、IDをそれぞれ画面（Model）に引き渡す
+		model.addAttribute("count", accounts.size()); // 2回SQLが走らないように最適化
+		model.addAttribute("accounts", accounts);
+		model.addAttribute("ids", ids);
 		return "deleted_account/export";
 	}
 	
 //エクスポートのダウンロード処理
-	@GetMapping("/export/download")
+	@PostMapping("/export/download")
 	public ResponseEntity<byte[]> downloadCsv(
 			@RequestParam(name = "ids", required = false) List<Integer> ids) {
 		List<Account> accounts = deletedAccountService.findByIds(ids);
 		StringBuilder csvBuilder = new StringBuilder("アカウントID,ログインID,権限,社員名(姓),社員名(名)\n");
 		for (Account acc : accounts) {
+			String lastName = "-";
+			String firstName = "-";
+			if (acc.getEmployee() != null) {
+				lastName = acc.getEmployee().getLastName();
+				firstName = acc.getEmployee().getFirstName();
+			}
+
 			csvBuilder.append(acc.getAccountId()).append(",")
 					.append(acc.getLoginId()).append(",")
 					.append(acc.getPermission()).append(",")
-					.append(acc.getEmployee().getLastName()).append(",")
-					.append(acc.getEmployee().getFirstName()).append("\n");
+					.append(lastName).append(",")
+					.append(firstName).append("\n");
 		}
 		byte[] csvBytes = csvBuilder.toString().getBytes(StandardCharsets.UTF_8);
 		byte[] bom = new byte[] { (byte) 0xEF, (byte) 0xBB, (byte) 0xBF };
@@ -143,5 +153,4 @@ public class DeletedAccountController {
 		headers.add("Content-Type", "text/csv; charset=UTF-8");
 		return new ResponseEntity<>(result, headers, HttpStatus.OK);
 	}
-
 }
