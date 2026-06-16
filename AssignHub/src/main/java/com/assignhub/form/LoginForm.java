d0608b0a8342d8b3f9376dd3954ba68b0d7dc62a
hsano <hsano@192.168.6.33>
2026-06-16 16:47:30 +0900

    ログイン機能の追加

---
+++ AssignHub/src/main/java/com/assignhub/form/LoginForm.java
@@ -0,0 +1,38 @@
+package com.assignhub.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class LoginForm {
+	@NotBlank(message = "ログインIDは必須です")
+    @Size(min = 5, max = 12,
+          message = "ログインIDは5文字以上12文字以内で入力してください")
+    @Pattern(regexp = "^[A-Za-z0-9]+$",
+             message = "ログインIDは半角英数字のみで入力してください")
+    private String loginId;
+
+    
+    @NotBlank(message = "パスワードは必須です")
+    @Size(min = 8, max = 20,
+          message = "パスワードは8文字以上20文字以内で入力してください")
+    @Pattern(regexp = "^[A-Za-z0-9@_]+$",
+             message = "パスワードは半角英数字または記号(\"@\",\"_\")のみで入力してください")
+    private String password;
+
+
+    public String getLoginId() {
+        return loginId;
+    }
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+	
+
+}
