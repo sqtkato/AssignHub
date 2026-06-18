597f6143495cb473d81705b605ea7882a49eb84a
c3s_218 <c3s_218@LAPTOP-Q9LDQPEP>
2026-06-18 13:17:16 +0900

    エクスポート時のファイル名修正

--- AssignHub/src/main/java/com/assignhub/controller/DeletedAccountController.java
+++ AssignHub/src/main/java/com/assignhub/controller/DeletedAccountController.java
@@ -154,7 +154,7 @@ public class DeletedAccountController {
 		System.arraycopy(bom, 0, result, 0, bom.length);
 		System.arraycopy(csvBytes, 0, result, bom.length, csvBytes.length);
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "attachment; filename=account.csv");
+		headers.add("Content-Disposition", "attachment; filename=deleted-accounts.csv");
 		headers.add("Content-Type", "text/csv; charset=UTF-8");
 		return new ResponseEntity<>(result, headers, HttpStatus.OK);
 	}
