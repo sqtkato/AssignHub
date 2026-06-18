f3f2e9fa7bd802f566afee1ea5dff5aa3152e78f
土手内奨 <@192.168.6.50>
2026-06-18 13:17:06 +0900

    ファイルネーム修正

--- AssignHub/src/main/java/com/assignhub/controller/DeletedEmployeeController.java
+++ AssignHub/src/main/java/com/assignhub/controller/DeletedEmployeeController.java
@@ -234,7 +234,7 @@ public class DeletedEmployeeController {
 		System.arraycopy(csvBytes, 0, result, bom.length, csvBytes.length);
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "attachment; filename=employees.csv");
+		headers.add("Content-Disposition", "attachment; filename=deleted-employees.csv");
 		headers.add("Content-Type", "text/csv; charset=UTF-8");
 		return new ResponseEntity<>(result, headers, HttpStatus.OK);
 	}
