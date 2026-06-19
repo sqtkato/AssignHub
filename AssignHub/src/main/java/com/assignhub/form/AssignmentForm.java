0515cec70d654ead83ba82e9e35968e9c5aca16e
NonCa <NonCa@LAPTOP-8RM03O58>
2026-06-19 13:37:13 +0900

    AssignmentForm 契約単価の修正 中村

--- AssignHub/src/main/java/com/assignhub/form/AssignmentForm.java
+++ AssignHub/src/main/java/com/assignhub/form/AssignmentForm.java
@@ -35,7 +35,7 @@ public class AssignmentForm {
 
     /** 契約単価 */
     @NotNull(message = "契約単価は必須です")
-    @DecimalMin(value = "1", message = "この値は入力できません")
+    @DecimalMin(value = "0", message = "この値は入力できません")
     @Digits(integer = 10, fraction = 0, message = "契約単価は10文字以内で入力してください")
     private BigDecimal unitPrice;
 
